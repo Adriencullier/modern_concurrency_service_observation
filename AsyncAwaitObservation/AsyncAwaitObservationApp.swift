737d3ae8ea09a218b08ec30785b66cc0c39f4dd8
Adriencullier <cullieradrien@gmail.com>
2024-08-29 20:25:53 +0200

    Initial Commit

---
+++ AsyncAwaitObservation/AsyncAwaitObservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitObservationApp.swift
+//  AsyncAwaitObservation
+//
+//  Created by Adrien Cullier on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitObservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
